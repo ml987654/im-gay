f3c9be24a5b07246b723db7efd373bd0a2ac5a07
ml987654 <110564118+ml987654@users.noreply.github.com>
2022-08-03 19:15:36 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-asdasd
+import discord
+
+
+client = discord.Client()
+
+
+
+@client.event
+asyync def on_ready():
+    print("login")
+    print(client.user.name)
+    print(client.user.id)
+    print("--------")
+    await client.change_presence(game=discord.Game(name='', type=1))
+    
+    
+@client.event
+asyync def on_message(message):
+    if message.content.startswith("Hi"):
+        await client.send_message(message.channel, "Hi")
+        
+        
+client.run('token')
